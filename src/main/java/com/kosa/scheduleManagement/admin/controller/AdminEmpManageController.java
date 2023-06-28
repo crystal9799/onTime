fdde5a2c5c369aca89ce6261a4cb87c4246760d4
Gunhee Lee <rjsgml9799@gmail.com>
2023-06-28 16:50:56 +0900

    Fix:getJSON

--- src/main/java/com/kosa/scheduleManagement/admin/controller/AdminEmpManageController.java
+++ src/main/java/com/kosa/scheduleManagement/admin/controller/AdminEmpManageController.java
@@ -3,16 +3,20 @@ package com.kosa.scheduleManagement.admin.controller;
 import com.kosa.scheduleManagement.admin.service.EmpManageService;
 import com.kosa.scheduleManagement.global.dto.Emp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/admin")
-@Controller
+@RestController
 public class AdminEmpManageController {
     private EmpManageService empManageService;
     
@@ -22,11 +26,15 @@ public class AdminEmpManageController {
     }
     
     @GetMapping
-    public List<Emp> empShow(@RequestParam("deptno") int deptno){
-    	System.out.println("empShow 실행");
-    	List<Emp> list = empManageService.getList(deptno);
-    	System.out.println("서비스 호출 성공");
-    	System.out.println(list.toString());
-    	return list;
+    public ResponseEntity<List<Emp>> empShow(@RequestParam("deptno") int deptno){
+    	List<Emp> list = new ArrayList();
+    	
+    	try {
+    		System.out.println("정상실행");
+    		list = empManageService.getList(deptno);
+    		return new ResponseEntity<List<Emp>>(list,HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<List<Emp>>(list,HttpStatus.BAD_REQUEST);
+		}
     }
 }
